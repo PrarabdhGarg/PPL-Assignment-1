ea6f45de8131726893833a4073f7b489328e745d
root <root@LAPTOP-P186MTEQ.localdomain>
2020-10-04 20:15:17 +0530

    Some random commit

---
+++ typeExpressionTable.h
@@ -0,0 +1,20 @@
+#include "typeExpression.h"
+
+typedef enum {
+    PrimitiveDataType,
+    RectangularArray,
+    JaggedArray
+} ArrayType;
+
+typedef enum {
+    Static,
+    Dynamic,
+    NotApplicable
+} TypeOfRange;
+
+typedef struct {
+    char *variableName;
+    ArrayType arrayType;
+    TypeOfRange typeOfRange;
+    TypeExpression typeExpression;
+} TypeExpressionTableElement;
