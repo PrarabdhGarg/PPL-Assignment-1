1012486ef6e86d41a20c0d5b2fbbc60657e27b35
root <root@LAPTOP-P186MTEQ.localdomain>
2020-10-07 23:42:30 +0530

    Define type Expression data type

--- typeExpression.h
+++ typeExpression.h
@@ -1,3 +1,39 @@
-typedef union {
+typedef enum {
+    Integer,
+    Real,
+    Boolean,
+    RectangularArray,
+    JaggedArray
+} DataTypes;
 
+typedef struct {
+    int start;
+    int end;
+} RectangularRange;
+
+typedef struct {
+    int size;
+    int *sizes;
+} JaggedRange;
+
+typedef struct {
+    int dimensions;
+    RectangularRange *ranges;
+    DataTypes basicElementType;
+} RectangularArrayTypeExpression;
+
+typedef struct {
+    int dimensions;
+    JaggedRange *ranges;
+    DataTypes basicDataType;
+} JaggedArrayTypeExpression;
+
+typedef union {
+    RectangularArrayTypeExpression rectangularArrayTypeExpression;
+    JaggedArrayTypeExpression jaggedArrayTypeExpression;
+} ArrayTypeExpression;
+
+typedef struct {
+    DataTypes type;
+    ArrayTypeExpression arrayTypeExpression;
 } TypeExpression;
